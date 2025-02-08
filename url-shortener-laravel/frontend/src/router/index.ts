c37b72b25bbd78e4003faba016bc65333515f215
Mateusz Maślanka <91078604+maslankamateusz@users.noreply.github.com>
2025-02-08 19:26:11 +0100

    chore: add Vue Router and initial routes

--- url-shortener-laravel/frontend/src/router/index.ts
+++ url-shortener-laravel/frontend/src/router/index.ts
@@ -1,23 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ShortenerView from "../views/ShortenerView.vue";
+import ExpandView from "../views/ExpandView.vue";
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/shorten", component: ShortenerView },
+  { path: "/expand", component: ExpandView },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
